0bfd3466719145e574a169ebace2fbcde26dc4d9
Richardkubik875 <243011@student.vitkovickastredni.cz>
2025-04-23 09:33:46 +0200

    Delete al phyton directory

--- al phyton/ostatní/fibo_posl.py
+++
@@ -1,9 +0,0 @@
-fibo_posl = [0, 1]
-pocet_cisel = 12
-
-while len(fibo_posl) < pocet_cisel:
-    fibo_posl.append(fibo_posl[-1] + fibo_posl[-2])
-
-vysledek = [cislo for cislo in fibo_posl if cislo < 100]
-
-print(vysledek)
